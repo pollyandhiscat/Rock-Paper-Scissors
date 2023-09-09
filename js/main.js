e0e08cc8bde3e191ec741d65c7530bb5d6f30cde
pollyandhiscat <pollyandhiscat@outlook.com>
2023-09-09 12:26:11 -0700

    Add logic to determine winner and return outcome

--- js/main.js
+++ js/main.js
@@ -23,8 +23,49 @@ function getPlayerChoice(){
 
 function determineWinner(computerChoice, playerChoice){
 
-    
+    if (computerChoice == playerChoice) {
 
+        result = `Computer chose ${computerChoice}. Player chose ${playerChoice}. Tie game.`;
+    }
+
+    else if (computerChoice == 'rock'){
+
+        if (playerChoice == 'paper'){
+            result = `Computer chose ${computerChoice}. Player chose ${playerChoice}. Player wins.`;
+        }
+
+        else {
+
+            result = `Computer chose ${computerChoice}. Player chose ${playerChoice}. Computer wins.`;
+        }
+    }
+
+    else if (computerChoice == 'paper'){
+
+        if (playerChoice == 'scissors'){
+            result = `Computer chose ${computerChoice}. Player chose ${playerChoice}. Player wins.`;
+        }
+
+        else {
+
+            result = `Computer chose ${computerChoice}. Player chose ${playerChoice}. Computer wins.`;
+        }   
+
+    }
+        
+    else if (computerChoice == 'scissors'){
+
+        if (playerChoice == 'rock'){
+            result = `Computer chose ${computerChoice}. Player chose ${playerChoice}. Player wins.`;
+        }
+
+        else {
+
+            result = `Computer chose ${computerChoice}. Player chose ${playerChoice}. Computer wins.`;
+        }
+    }
+
+    return result;
 }
 
 
