0fb2d4eb7d6bd07cbd49af2b554ca7572f625c63
pollyandhiscat <pollyandhiscat@outlook.com>
2023-09-09 12:29:52 -0700

    Remove console.log statement from computerChoice()

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ function getComputerChoice(){
 
     let choices = ['rock', 'paper', 'scissors'];
     let choice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(choice);
 
 }
 
